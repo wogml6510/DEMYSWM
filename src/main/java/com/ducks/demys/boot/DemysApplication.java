e81486b6faf906bf12b45e1289bfade6cb7ca702
OSSAONE\dhtkd <dhtkd4900@gmail.com>
2023-04-29 12:06:56 +0900

    최종프로젝트 첫 커밋

---
+++ src/main/java/com/ducks/demys/boot/DemysApplication.java
@@ -0,0 +1,13 @@
+package com.ducks.demys.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ducks/demys/boot/DemysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducks.demys.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
